7a5bc680e0927bacfbcf4439fa0620bf8d77fada
Azamat225 <azamat_rakhmatullin@bk.ru>
2025-09-22 21:59:12 +0500

    pushhhh22222

--- manager2/views.py
+++ manager2/views.py
@@ -994,20 +994,14 @@ def employee_shifts(request, user_id):
     # Если месяц не выбран, используем текущий месяц
     if selected_month_str:
         try:
-            selected_month = dt.datetime.strptime(selected_month_str + '-01', '%Y-%m-%d').date()
+            # создаём naive datetime
+            selected_month_naive = dt.datetime.strptime(selected_month_str + '-01', '%Y-%m-%d')
+            # делаем его aware в текущей TZ
+            selected_month = timezone.make_aware(selected_month_naive)
         except (ValueError, TypeError):
-            selected_month = timezone.now().date()
+            selected_month = timezone.now()
     else:
-        selected_month = timezone.now().date()
-
-    # Делаем first/last как date
-    first_day_of_month = timezone.make_aware(dt.datetime.combine(selected_month.replace(day=1), dt.time.min))
-    if selected_month.month == 12:
-        last_day_of_month = timezone.make_aware(
-            dt.datetime.combine(selected_month.replace(year=selected_month.year + 1, month=1, day=1), dt.time.min))
-    else:
-        last_day_of_month = timezone.make_aware(
-            dt.datetime.combine(selected_month.replace(month=selected_month.month + 1, day=1), dt.time.min))
+        selected_month = timezone.now()
 
     # Определяем первый и последний день выбранного месяца
     first_day_of_month = selected_month.replace(day=1)
